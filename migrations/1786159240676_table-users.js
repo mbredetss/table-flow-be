ec3afe541b78ba7b67a32bf56bf7785ccd3c5dc2
mbredets <mr.mighdadjaba@gmail.com>
2026-08-08 12:47:31 +0800

    fix(auth): add users chema

---
+++ migrations/1786159240676_table-users.js
@@ -0,0 +1,56 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = async (pgm) => {
+    pgm.createType('user_role', ['admin']);
+
+    pgm.createTable('users', {
+        id: {
+            type: 'CHAR(21)',
+            primaryKey: true
+        },
+        username: {
+            type: 'VARCHAR(100)',
+            notNull: true,
+            unique: true,
+        },
+        password: {
+            type: 'TEXT',
+            notNull: true,
+        },
+        role: {
+            type: 'user_role',
+            notNull: true,
+        },
+        fullName: {
+            type: 'VARCHAR(140)',
+            notNull: true
+        },
+        createdAt: {
+            type: 'TIMESTAMPTZ',
+            notNull: true,
+            default: pgm.func('CURRENT_TIMESTAMP')
+        },
+        updatedAt: {
+            type: 'TIMESTAMPTZ',
+            notNull: true,
+            default: pgm.func('CURRENT_TIMESTAMP')
+        }
+    });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+    pgm.dropTable('users');
+};
