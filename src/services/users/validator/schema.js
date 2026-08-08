853e7c4e291b71d5d56446eb3f99139be4143354
mbredets <mr.mighdadjaba@gmail.com>
2026-08-08 13:15:07 +0800

    reafactor: delete users services

--- src/services/users/validator/schema.js
+++
@@ -1,7 +0,0 @@
-import Joi from 'joi';
-
-export const userPayloadSchema = Joi.object({
-  username: Joi.string().required(),
-  password: Joi.string().required(),
-  fullname: Joi.string().required(),
-});
