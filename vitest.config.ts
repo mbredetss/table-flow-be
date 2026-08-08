29beef60fd6d072148bd87e62643645470a1666c
mbredets <mr.mighdadjaba@gmail.com>
2026-08-08 14:49:55 +0800

    update vitest config

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitest/config';
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({
+  path: path.resolve(process.cwd(), '.test.env'),
+});
+
+export default defineConfig({
+  test: {
+    globals: true,
+  },
+});
