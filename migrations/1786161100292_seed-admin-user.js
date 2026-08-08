01d94277fc4d69773073de6c6bdf54416df06c4a
mbredets <mr.mighdadjaba@gmail.com>
2026-08-08 12:47:52 +0800

    fix(auth): add admin users

---
+++ migrations/1786161100292_seed-admin-user.js
@@ -0,0 +1,33 @@
+import bcrypt from 'bcrypt';
+import { nanoid } from 'nanoid';
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = async (pgm) => {
+    const adminId = `user-${nanoid(16)}`;
+    const adminUsername = process.env.ADMIN_USERNAME;
+    const hashedPassword = await bcrypt.hash(process.env.ADMIN_PASSWORD, 10);
+    const role = 'admin';
+    const fullName = 'Admin Kasir';
+
+    await pgm.db.query(
+        `INSERT INTO users VALUES ($1, $2, $3, $4, $5)`,
+        [adminId, adminUsername, hashedPassword, role, fullName]);
+
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+    pgm.sql(`DELETE from users WHERE username = ${process.env.ADMIN_USERNAME}`);
+};
